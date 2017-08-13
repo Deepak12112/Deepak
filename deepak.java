d8b10ba6faece77abbf374db63f017472c02d52b
Deepak12112 <30657734+Deepak12112@users.noreply.github.com>
2017-08-13 04:15:04 -0700

    scan value

--- deepak.java
+++ deepak.java
@@ -1,7 +1,13 @@
+import java.util.*;
 class Deepak
 {
  public static void main(String args[])
  {
+  Scanner sc = new Scanner(System.in);
+  String str=sc.nextLine();
+  
+   System.out.println(str);
+  
    System.out.println("Hello Deepak");
   
    System.out.println("Hello Friends");
